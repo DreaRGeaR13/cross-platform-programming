bae96c82384c12062c47428299a1ed9ab92c8b2f
Vladimir Odinskiy <70931401+DreaRGeaR13@users.noreply.github.com>
2022-10-19 22:31:29 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+import math
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    a = int(input('Введите A: '))
+    b = int(input('Введите B: '))
+    x = int(input('Введите X: '))
+    if x >= 8:
+        print("X больше либо равен 8")
+        y = ((a * a) + 4*(x*x) + b) / a*b
+    else:
+        print("X меньше 8")
+        y = (a * a) - 2*(x*x)
+    print("y = %.2f" % y)
+
+
+if __name__ == '__main__':
+    main()
